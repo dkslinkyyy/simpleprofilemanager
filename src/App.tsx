6626c35337a9e148a162614dacc4de3517b13b3b
dkslinkyyy <slinky.sthlm@gmail.com>
2025-02-13 14:25:18 +0100

    14:25

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Navigation from "./components/Navigation";
